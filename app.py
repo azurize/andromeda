ec61aaebae7c985e4e495366fed9f1b54fcdb6c1
David Grabosky <david.grabosky0401@gmail.com>
2022-06-10 13:54:24 -0700

    Removed MongoDB and added Postgres integration

--- app.py
+++ app.py
@@ -1,58 +1,132 @@
 from datetime import timedelta
 from dotenv import load_dotenv
 from flask import Flask, flash, render_template, request, redirect, url_for, session
-from pymongo import MongoClient
 
-import primetrust
 import bcrypt
+import logging
 import os
+import psycopg2
 
 load_dotenv()
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='{asctime} {levelname:<8} {message}',
+    style='{'
+)
+
 
 def create_app():
     app = Flask(__name__)
     app.secret_key = os.urandom(24)
-    client = MongoClient(os.environ.get('MONGODB_URI'))
-    app.db = client.andromeda
+
+    class User:
+        def __init__(self, email, password, name) -> None:
+            self.email = email
+            self.password = password
+            self.name = name
+
+    # Connection to Postgres DB
+    def get_db_connection():
+        connection = psycopg2.connect(
+            user=os.environ.get('DB_USER'),
+            password=os.environ.get('DB_PASS'),
+            host=os.environ.get('DB_HOST'),  # localhost used during test phase
+            database=os.environ.get('DB_NAME')
+        )
+        return connection
 
     @app.before_request
     def makeSessionPermanent():
         session.permanent = True
         app.permanent_session_lifetime = timedelta(minutes=10)
 
-    @app.route('/')
+    @app.route('/', methods=['GET', 'POST'])
     def index():
         if 'email' in session:
             return render_template('index.html')
-        else: return redirect(url_for('login'))
+        else:
+            return redirect(url_for('login'))
 
+    # User login page
     @app.route('/login/', methods=['GET', 'POST'])
     def login():
         if request.method == 'POST':
-            users = app.db.users
-            login_user = users.find_one({'email' : request.form['email']})
-            password = request.form['password'].encode('utf-8')
+            user = User(
+                email=request.form['email'],
+                password=request.form['password'],
+                name=None
+            )
 
-            if login_user:
-                if bcrypt.checkpw(password, login_user['password']):
-                    session['email'] = request.form['email']
+            connection = get_db_connection()
+            cursor = connection.cursor()
+
+            try:
+                sql = 'SELECT password FROM users WHERE username = %s;'
+                params = [user.email]
+                cursor.execute(sql, params)
+            except Exception as e:
+                logging.critical('Failed with the following exception: ', e)
+
+            login = cursor.fetchall()
+            output = str([x[0] for x in login])
+            password = output.strip("'[]")
+
+            if login != []:
+                if bcrypt.checkpw(user.password.encode('utf-8'), password.encode('utf-8')):
+                    session['email'] = user.email
                     return redirect(url_for('index'))
-            else: flash('Incorrect email/password combination!')
-                
+                else:
+                    flash('Email/password combination does not exist.')
+            else:
+                flash('Email/password combination does not exist.')
+
         return render_template('login.html')
 
+    # User creation page
     @app.route('/create/', methods=['GET', 'POST'])
     def create():
         if request.method == 'POST':
-            users = app.db.users
-            existing_user = users.find_one({'email' : request.form['email']})
+            user = User(
+                email=request.form['email'],
+                password=request.form['password'],
+                name=request.form['name']
+            )
 
-            if existing_user is None:
-                hashed_pword = bcrypt.hashpw(request.form['password'].encode('utf-8'), bcrypt.gensalt())
-                users.insert({'name' : request.form['name'], 'email' : request.form['email'], 'password' : hashed_pword})
-                session['email'] = request.form['email']
+            connection = get_db_connection()
+            cursor = connection.cursor()
+
+            try:
+                sql = 'SELECT username FROM users WHERE username = (%s);'
+                params = [user.email]
+                cursor.execute(sql, params)
+            except Exception as e:
+                logging.critical('Failed with the following exception: ', e)
+
+            output = cursor.fetchall()
+
+            if output == []:
+                email = user.email
+                password = bcrypt.hashpw(
+                    user.password.encode('utf-8'), bcrypt.gensalt())
+                # Decoded for proper db storage - if not decoded, stored value causes issues with bcrypt checkpw for login
+                password_decode = password.decode('utf-8')
+                name = user.name
+
+                sql = 'INSERT INTO users (username, password, name) VALUES (%s, %s, %s)'
+                params = (email, password_decode, name)
+                cursor.execute(sql, params)
+
+                # Commit transaction to db
+                connection.commit()
+
+                # Close connection to db
+                connection.close()
+
+                session['email'] = email
                 return redirect(url_for('index'))
-            else: flash('That email is already registered.')
+            else:
+                flash('That email is already registered.')
 
         return render_template('create.html')
 
